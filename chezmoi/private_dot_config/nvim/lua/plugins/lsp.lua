bbcda15123f6201b1a42aadc74d892ca888796f3
Brad Ellis <brad@ellis.jp>
2026-01-25 09:40:43 +0900

    feat: fix dircolors, default vimdiff to nvim -d add shellcheck to nvim

--- chezmoi/private_dot_config/nvim/lua/plugins/lsp.lua
+++ chezmoi/private_dot_config/nvim/lua/plugins/lsp.lua
@@ -1,24 +1,39 @@
 return {
   {
-  'williamboman/mason-lspconfig.nvim',
-  dependencies = {
-    'williamboman/mason.nvim',
-    'neovim/nvim-lspconfig'
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    config = function()
+      require("mason").setup()
+
+      require("mason-tool-installer").setup({
+        ensure_installed = {
+          "shellcheck",
+        },
+        run_on_start = true,
+      })
+    end,
   },
-  config = function()
-    require('mason').setup()
-    require('mason-lspconfig').setup({
-      ensure_installed = { 'clangd', 'lua_ls', 'docker_compose_language_service', 'yamlls', 'pylsp' }
-    })
 
-    vim.lsp.enable('yamlls')
-    vim.lsp.enable('pylsp')
-    vim.lsp.enable('clangd')
-    vim.lsp.enable('lua_ls')
-    vim.lsp.enable('docker_compose_language_service')
+  {
+    'williamboman/mason-lspconfig.nvim',
+    dependencies = {
+      'williamboman/mason.nvim',
+      'neovim/nvim-lspconfig'
+    },
+    config = function()
+      require('mason').setup()
+      require('mason-lspconfig').setup({
+        ensure_installed = { 'clangd', 'lua_ls', 'docker_compose_language_service', 'yamlls', 'pylsp' }
+      })
 
-    -- This isn't related to mason, but is setup as a local custom lspconfig
-    vim.lsp.enable('likec4_ls')
+      vim.lsp.enable('yamlls')
+      vim.lsp.enable('pylsp')
+      vim.lsp.enable('clangd')
+      vim.lsp.enable('lua_ls')
+      vim.lsp.enable('docker_compose_language_service')
+
+      -- This isn't related to mason, but is setup as a local custom lspconfig
+      vim.lsp.enable('likec4_ls')
     end
   },
 
